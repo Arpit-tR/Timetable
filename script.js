e14c527d38a7adc9149124dd9de461fa1fa7fd42
Arpit Sadhukhan <arpitsadhukhan@gmail.com>
2025-04-21 22:45:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,288 +1,43 @@
 const timetable1 = {
-  Monday: [
-    "Free",
-    "Free",
-    "Free",
-    "Break",
-    "3C",
-    "3C",
-    "Break",
-    "3D",
-    "3D",
-    "Free",
-    "Free",
-  ],
-  Tuesday: [
-    "Free",
-    "3B",
-    "3B",
-    "Break",
-    "3E",
-    "3E",
-    "Break",
-    "Free",
-    "Free",
-    "Free",
-    "Free",
-  ],
-  Wednesday: [
-    "Free",
-    "3G",
-    "3G",
-    "Break",
-    "Free",
-    "Free",
-    "Break",
-    "Free",
-    "Free",
-    "4A",
-    "4A",
-  ],
-  Thursday: [
-    "Free",
-    "4D",
-    "4D",
-    "Break",
-    "Free",
-    "Free",
-    "Break",
-    "3A",
-    "3A",
-    "4B",
-    "4B",
-  ],
-  Friday: [
-    "Free",
-    "4C",
-    "4C",
-    "Break",
-    "3F",
-    "3F",
-    "Break",
-    "Free",
-    "Free",
-    "Free",
-    "Free",
-  ],
+  Monday: ["Free", "Free", "Break", "3C", "Break", "3D", "Free"],
+  Tuesday: ["Free", "3B", "Break", "3E", "Break", "Free", "Free"],
+  Wednesday: ["Free", "3G", "Break", "Free", "Break", "Free", "4A"],
+  Thursday: ["Free", "4D", "Break", "Free", "Break", "3A", "4B"],
+  Friday: ["Free", "4C", "Break", "3F", "Break", "Free", "Free"],
 };
 
 const timetable2 = {
-  Monday: [
-    "Free",
-    "5E",
-    "5E",
-    "Break",
-    "Free",
-    "Free",
-    "Break",
-    "Free",
-    "Free",
-    "5F",
-    "5F",
-  ],
-  Tuesday: [
-    "Free",
-    "5C",
-    "5C",
-    "Break",
-    "Free",
-    "Free",
-    "Break",
-    "Free",
-    "Free",
-    "5B",
-    "5B",
-  ],
-  Wednesday: [
-    "Free",
-    "4F",
-    "4F",
-    "Break",
-    "Free",
-    "Free",
-    "Break",
-    "4G",
-    "4G",
-    "Free",
-    "Free",
-  ],
-  Thursday: [
-    "Free",
-    "Free",
-    "Free",
-    "Break",
-    "5A",
-    "5A",
-    "Break",
-    "Free",
-    "Free",
-    "5G",
-    "5G",
-  ],
-  Friday: [
-    "Free",
-    "5D",
-    "5D",
-    "Break",
-    "5H",
-    "5H",
-    "Break",
-    "Free",
-    "Free",
-    "4E",
-    "4E",
-  ],
+  Monday: ["Free", "5E", "Break", "Free", "Break", "Free", "5F"],
+  Tuesday: ["Free", "5C", "Break", "Free", "Break", "Free", "5B"],
+  Wednesday: ["Free", "4F", "Break", "Free", "Break", "4G", "Free"],
+  Thursday: ["Free", "Free", "Break", "5A", "Break", "Free", "5G"],
+  Friday: ["Free", "5D", "Break", "5H", "Break", "Free", "4E"],
 };
 
 const timetable3 = {
-  Monday: [
-    "Free",
-    "6G",
-    "6G",
-    "Break",
-    "Free",
-    "Free",
-    "Break",
-    "6F",
-    "6F",
-    "Free",
-    "Free",
-  ],
-  Tuesday: [
-    "Free",
-    "Free",
-    "Free",
-    "Break",
-    "6D",
-    "6D",
-    "Break",
-    "6E",
-    "6E",
-    "7A",
-    "7A",
-  ],
-  Wednesday: [
-    "Free",
-    "6H",
-    "6H",
-    "Break",
-    "8E",
-    "8E",
-    "Break",
-    "6A",
-    "6A",
-    "7B",
-    "7B",
-  ],
-  Thursday: [
-    "Free",
-    "Free",
-    "Break",
-    "6B",
-    "6B",
-    "Break",
-    "Free",
-    "Free",
-    "Free",
-    "Free",
-  ],
-  Friday: [
-    "Free",
-    "7C",
-    "7C",
-    "Break",
-    "Free",
-    "Free",
-    "Break",
-    "6C",
-    "6C",
-    "Free",
-    "Free",
-  ],
+  Monday: ["Free", "6G", "Break", "Free", "Break", "6F", "Free"],
+  Tuesday: ["Free", "Free", "Break", "6D", "Break", "6E", "7A"],
+  Wednesday: ["Free", "6H", "Break", "8E", "Break", "6A", "7B"],
+  Thursday: ["Free", "Break", "6B", "Break", "Free", "Free"],
+  Friday: ["Free", "7C", "Break", "Free", "Break", "6C", "Free"],
 };
 
 const timetable4 = {
-  Monday: [
-    "Free",
-    "7D",
-    "7D",
-    "Break",
-    "7G",
-    "7G",
-    "Break",
-    "Free",
-    "Free",
-    "7E",
-    "7E",
-  ],
-  Tuesday: [
-    "Free",
-    "8B",
-    "8B",
-    "Break",
-    "8F",
-    "8F",
-    "Break",
-    "Free",
-    "Free",
-    "8G",
-    "8G",
-  ],
-  Wednesday: [
-    "Free",
-    "Free",
-    "Free",
-    "Break",
-    "7F",
-    "7F",
-    "Break",
-    "8A",
-    "8A",
-    "Free",
-    "Free",
-  ],
-  Thursday: [
-    "Free",
-    "7H",
-    "7H",
-    "Break",
-    "Free",
-    "Free",
-    "Break",
-    "8D",
-    "8D",
-    "Free",
-    "Free",
-  ],
-  Friday: [
-    "Free",
-    "8C",
-    "8C",
-    "Break",
-    "8H",
-    "8H",
-    "Break",
-    "Free",
-    "Free",
-    "Free",
-    "Free",
-  ],
+  Monday: ["Free", "7D", "Break", "7G", "Break", "Free", "7E"],
+  Tuesday: ["Free", "8B", "Break", "8F", "Break", "Free", "8G"],
+  Wednesday: ["Free", "Free", "Break", "7F", "Break", "8A", "Free"],
+  Thursday: ["Free", "7H", "Break", "Free", "Break", "8D", "Free"],
+  Friday: ["Free", "8C", "Break", "8H", "Break", "Free", "Free"],
 };
 
 const periodTimes = [
   ["8:10 AM", "8:50 AM"],
-  ["8:50 AM", "9:30 AM"],
-  ["9:30 AM", "10:10 AM"],
-  // Break
+  ["8:50 AM", "10:10 AM"],
   ["10:10 AM", "10:20 AM"], // Break period
-  ["10:20 AM", "11:00 AM"],
-  ["11:00 AM", "11:40 AM"],
-  // Break
+  ["10:20 AM", "11:40 AM"],
   ["11:40 AM", "12:00 PM"], // Break period
-  ["12:00 PM", "12:40 PM"],
-  ["12:40 PM", "1:20 PM"],
-  ["1:20 PM", "2:00 PM"],
-  ["2:00 PM", "2:35 PM"], // Updated end time for the last period
+  ["12:00 PM", "1:20 PM"],
+  ["1:20 PM", "2:40 PM"], // Updated end time for the last period
 ];
 
 function getCurrentPeriodIndex() {
@@ -353,45 +108,67 @@ function updateBoxes() {
         ? currentInfo.periodIndex + 1
         : currentInfo.periodIndex;
 
-    output1 = `Day: ${currentInfo.day} <br> Period: ${
-      currentInfo.periodIndex + 1
-    } <br> Class: ${
+    // Determine the period display based on the current period index
+    const periodDisplay = (index) => {
+      switch (index) {
+        case 0:
+          return "1"; // 1st period
+        case 1:
+          return "2+3"; // 2nd period
+        case 2:
+          return "Break"; // 3rd period
+        case 3:
+          return "4+5"; // 4th period
+        case 4:
+          return "Break"; // 5th period
+        case 5:
+          return "6+7"; // 6th period
+        case 6:
+          return "8+9"; // 7th period
+        default:
+          return ""; // Default case
+      }
+    };
+
+    output1 = `Day: ${currentInfo.day} <br> Period: ${periodDisplay(
+      currentInfo.periodIndex
+    )} <br> Class: ${
       timetable1[currentInfo.day][currentInfo.periodIndex] || "Free Period"
     }`;
-    output2 = `Day: ${currentInfo.day} <br> Period: ${
-      currentInfo.periodIndex + 1
-    } <br> Class: ${
+    output2 = `Day: ${currentInfo.day} <br> Period: ${periodDisplay(
+      currentInfo.periodIndex
+    )} <br> Class: ${
       timetable2[currentInfo.day][currentInfo.periodIndex] || "Free Period"
     }`;
-    output3 = `Day: ${currentInfo.day} <br> Period: ${
-      currentInfo.periodIndex + 1
-    } <br> Class: ${
+    output3 = `Day: ${currentInfo.day} <br> Period: ${periodDisplay(
+      currentInfo.periodIndex
+    )} <br> Class: ${
       timetable3[currentInfo.day][currentInfo.periodIndex] || "Free Period"
     }`;
-    output4 = `Day: ${currentInfo.day} <br> Period: ${
-      currentInfo.periodIndex + 1
-    } <br> Class: ${
+    output4 = `Day: ${currentInfo.day} <br> Period: ${periodDisplay(
+      currentInfo.periodIndex
+    )} <br> Class: ${
       timetable4[currentInfo.day][currentInfo.periodIndex] || "Free Period"
     }`;
 
     // Update class timings and upcoming classes
-    const nextClassTiming1 = periodTimes[nextClassIndex][0];
-    const nextClassTiming2 = periodTimes[nextClassIndex][0];
-    const nextClassTiming3 = periodTimes[nextClassIndex][0];
-    const nextClassTiming4 = periodTimes[nextClassIndex][0];
+    const nextClassTiming1 = periodTimes[nextClassIndex];
+    const nextClassTiming2 = periodTimes[nextClassIndex];
+    const nextClassTiming3 = periodTimes[nextClassIndex];
+    const nextClassTiming4 = periodTimes[nextClassIndex];
 
     document.getElementById(
       "timing1"
-    ).innerHTML = `Class Timing: ${nextClassTiming1}`;
+    ).innerHTML = `Class Timing: ${nextClassTiming1[0]} - ${nextClassTiming1[1]}`;
     document.getElementById(
       "timing2"
-    ).innerHTML = `Class Timing: ${nextClassTiming2}`;
+    ).innerHTML = `Class Timing: ${nextClassTiming2[0]} - ${nextClassTiming2[1]}`;
     document.getElementById(
       "timing3"
-    ).innerHTML = `Class Timing: ${nextClassTiming3}`;
+    ).innerHTML = `Class Timing: ${nextClassTiming3[0]} - ${nextClassTiming3[1]}`;
     document.getElementById(
       "timing4"
-    ).innerHTML = `Class Timing: ${nextClassTiming4}`;
+    ).innerHTML = `Class Timing: ${nextClassTiming4[0]} - ${nextClassTiming4[1]}`;
 
     document.getElementById("upcomingClass1").innerHTML = `Upcoming Class: ${
       timetable1[currentInfo.day][nextClassIndex] || "No Class"
@@ -408,16 +185,16 @@ function updateBoxes() {
 
     document.getElementById(
       "timeLeft1"
-    ).innerHTML = `Time Left: ${calculateTimeLeft(nextClassTiming1)} min`;
+    ).innerHTML = `Time Left: ${calculateTimeLeft(nextClassTiming1[0])} min`;
     document.getElementById(
       "timeLeft2"
-    ).innerHTML = `Time Left: ${calculateTimeLeft(nextClassTiming2)} min`;
+    ).innerHTML = `Time Left: ${calculateTimeLeft(nextClassTiming2[0])} min`;
     document.getElementById(
       "timeLeft3"
-    ).innerHTML = `Time Left: ${calculateTimeLeft(nextClassTiming3)} min`;
+    ).innerHTML = `Time Left: ${calculateTimeLeft(nextClassTiming3[0])} min`;
     document.getElementById(
       "timeLeft4"
-    ).innerHTML = `Time Left: ${calculateTimeLeft(nextClassTiming4)} min`;
+    ).innerHTML = `Time Left: ${calculateTimeLeft(nextClassTiming4[0])} min`;
   }
 
   document.getElementById("output1").innerHTML = output1;
@@ -425,6 +202,22 @@ function updateBoxes() {
   document.getElementById("output3").innerHTML = output3;
   document.getElementById("output4").innerHTML = output4;
 }
+
+document.getElementById("box1").addEventListener("click", function () {
+  window.open("arpit.html", "_blank");
+});
+
+document.getElementById("box2").addEventListener("click", function () {
+  window.open("timetables/bicky.html", "_blank");
+});
+
+document.getElementById("box3").addEventListener("click", function () {
+  window.open("timetables/debmalya.html", "_blank");
+});
+
+document.getElementById("box4").addEventListener("click", function () {
+  window.open("timetables/dipankar.html", "_blank");
+});
 
 updateBoxes();
 setInterval(updateBoxes, 60000); // Update every minute
